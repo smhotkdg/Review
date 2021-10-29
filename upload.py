d9a6c85ad44d9c744d2ac06908bf1d95a2505441
smhotkdg@naver.com <cozy0614@gmail.com>
2021-10-29 17:06:30 +0900

    init

--- upload.py
+++ upload.py
@@ -19,4 +19,5 @@ def calculate(num=None):
     #return test.predictWeb(temp1)
 if __name__ == '__main__':
     #app.run()
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)    
+    #
