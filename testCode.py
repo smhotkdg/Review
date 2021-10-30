bae695d4ea60a91e2a994e69adb7f4ec46d51b5c
smhotkdg@naver.com <cozy0614@gmail.com>
2021-10-30 14:47:57 +0900

    확률 추가

--- testCode.py
+++ testCode.py
@@ -1,23 +1,6 @@
-from numpy import add
-
-students = [
-        ('홍길동', 3.9),
-        ('김철수', 3.0),
-        ('최자영', 4.3),]
-def appendNew():
-    students.append(('손지원',5))
-
-
-#sorted(students, key=lambda student: student[2])
-appendNew()
-sortresult = sorted(students, key=lambda percnet: percnet[1],reverse=True)
-
-def sortNum(num,_array):
-    resultArray = []
-    for i in range(len(_array)):
-        if(_array[i][1] > num):
-            resultArray.append(_array[i])
-    resultArray = sorted(resultArray, key=lambda percnet: percnet[1],reverse=True)
-    print(resultArray)
-
-sortNum(4.0,students)
+import re
+text = "이렇게 먹으면ㅋㅋㅋㅋ 살걱정없어욧"
+pattern = '([ㄱ-ㅎㅏ-ㅣ])'
+repl = ''
+result = re.sub(pattern=pattern,repl=repl,string=text)
+print(result)
