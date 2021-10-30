ed2a264f4078c873b02987e65c9ce4c8a68d0b5a
smhotkdg@naver.com <cozy0614@gmail.com>
2021-10-30 14:47:57 +0900

    확률 추가

--- upload.py
+++ upload.py
@@ -16,27 +16,29 @@ reviewData = []
 @app.route('/')
 def hello_world():
     return render_template("submit.html", len = len(reviewData), result_review_value = reviewData)
-@app.route('/calculate', methods=['POST', 'GET'])
-def calculate(num=None):
+@app.route('/reviewanalysis', methods=['POST', 'GET'])
+def analysis(num=None):
     temp1 = request.args.get('char1')
     #print(temp1)
-    #result1,resultText = test.predictWeb(temp1)
-    #return render_template('submit.html', result = result1, result2 = resultText)
-    return render_template('submit.html', result = temp1, result2 = temp1)    
+    result1,resultText = test.predictWeb(temp1)
+    return render_template('submit.html', result = result1, result2 = str(resultText),len = len(reviewData), result_review_value = reviewData)
+    #return render_template('submit.html', result = temp1, result2 = temp1)    
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def uploader_file():
+    
     if request.method == 'POST':
         file = request.files['file']
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))        
         filePath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)       
         
-        return fileRead()
+        return fileRead(filePath)
         
-def fileRead():    
-    df = pd.read_excel('D:/files/test.xlsx', engine="openpyxl")
+def fileRead(filePath):    
+    df = pd.read_excel(filePath, engine="openpyxl")
     series = df["CONTENTS"]
     series.head()
+    reviewData.clear()    
     for i in range(len(series)):
         strLine =series[i].replace("\n","")
         strLine = re.sub(' +', ' ', strLine)
@@ -48,10 +50,17 @@ def fileRead():
     reviewData_Sort = sortNum(40.0,reviewData)
     return render_template('submit.html', len = len(reviewData_Sort), result_review_value = reviewData_Sort)
 
+@app.route('/serchpercent', methods=['POST', 'GET'])
+def serchpercent(num=None):
+    temp1 = request.args.get('charPercet')
+    floatStr = float(temp1)
+    reviewData_Sort = sortNum(floatStr,reviewData)
+    return render_template('submit.html', len = len(reviewData_Sort), result_review_value = reviewData_Sort)
+
 def sortNum(num,_array):
     resultArray = []
     for i in range(len(_array)):
-        if(_array[i][1] > num):
+        if(_array[i][1] >= num):
             resultArray.append(_array[i])
     resultArray = sorted(resultArray, key=lambda percnet: percnet[1],reverse=True)   
     return resultArray
